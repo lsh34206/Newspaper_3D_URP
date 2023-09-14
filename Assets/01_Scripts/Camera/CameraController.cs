c1616811c2d8eb6ae53617fdabb051081a7cd42f
lsh34206 <30854829+lsh34206@users.noreply.github.com>
2023-09-14 12:11:18 +0900

    Revert "script added from main"
    This reverts commit 0adc6f4755f9bc2277105ec58013f8518637c9b1, reversing
    changes made to 4f03786ad0ab6c5d57b722c86ab40ffc9dd3342f.

--- Assets/01_Scripts/Camera/CameraController.cs
+++ Assets/01_Scripts/Camera/CameraController.cs
@@ -8,14 +8,6 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Vector2 turn;
     [SerializeField] private float sensitivity = .5f;
 
-    enum CameraPosition
-    {
-        Couch,
-        Table,
-        Bookshelf
-    }
-
-    private CameraPosition _state = CameraPosition.Couch;
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -23,19 +15,15 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        switch (_state)
+        RotateCamera();
+        if (Input.GetMouseButtonDown(0))
         {
-            case CameraPosition.Couch:
-                RotateCamera();
-                break;
-            
-            case CameraPosition.Table:
-                Cursor.lockState = CursorLockMode.Confined;
-                break;
-            case CameraPosition.Bookshelf:
-                Cursor.lockState = CursorLockMode.Confined;
-                break;
+            if (LookGameObject(out RaycastHit hit))
+            {
+                Destroy(hit.transform.gameObject);
+            }
         }
+
     }
 
     private void RotateCamera()
@@ -43,5 +31,11 @@ public class CameraController : MonoBehaviour
         turn.x += Input.GetAxis("Mouse X") * sensitivity;
         turn.y += Input.GetAxis("Mouse Y") * sensitivity;
         transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+    }
+
+    private bool LookGameObject(out RaycastHit hit)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        return Physics.Raycast(ray, out hit);
     }
 }
